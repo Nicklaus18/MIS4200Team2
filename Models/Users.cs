73f13d6bc3b94d6037956dc5dd6292bafc990f7f
Drew Wagnild <dw242218@ohio.edu>
2022-03-02 10:18:31 -0500

    user model

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,5 +7,20 @@ namespace MIS4200Team2.Models
 {
     public class Users
     {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string fullName
+        { 
+            get
+            {
+                return firstName + ", " + lastName;
+            }
+        }
+
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime registeredDate { get; set; }
+
     }
 }
