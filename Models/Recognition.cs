b1461abae3cd039e25264950557c90e4fd9fd936
Drew Wagnild <dw242218@ohio.edu>
2022-03-18 11:47:07 -0400

    changes

--- Models/Recognition.cs
+++ Models/Recognition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace MIS4200Team2.Models
     public class Recognition
     {
         public int RecognitionID { get; set; }
+        [Display(Name = "User")]
         public int userID { get; set; }
+        [Display(Name = "Core Value")]
         public int ValueID { get; set; }
 
         public virtual CoreValues CoreValues { get; set; }
