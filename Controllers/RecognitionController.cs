763c05e0596794b4c32380595dfdba40b1c46e9d
Drew Wagnild <dw242218@ohio.edu>
2022-03-18 10:53:40 -0400

    Recognition controller and other

---
+++ Controllers/RecognitionController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200Team2.DAL;
+using MIS4200Team2.Models;
+
+namespace MIS4200Team2.Controllers
+{
+    public class RecognitionController : Controller
+    {
+        private MIS4200Team2Context db = new MIS4200Team2Context();
+
+        // GET: Recognition
+        public ActionResult Index()
+        {
+            return View(db.Recognitions.ToList());
+        }
+
+        // GET: Recognition/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recognition recognition = db.Recognitions.Find(id);
+            if (recognition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recognition);
+        }
+
+        // GET: Recognition/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recognition/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RecognitionID,userID,ValueID")] Recognition recognition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Recognitions.Add(recognition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recognition);
+        }
+
+        // GET: Recognition/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recognition recognition = db.Recognitions.Find(id);
+            if (recognition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recognition);
+        }
+
+        // POST: Recognition/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RecognitionID,userID,ValueID")] Recognition recognition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recognition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recognition);
+        }
+
+        // GET: Recognition/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recognition recognition = db.Recognitions.Find(id);
+            if (recognition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recognition);
+        }
+
+        // POST: Recognition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recognition recognition = db.Recognitions.Find(id);
+            db.Recognitions.Remove(recognition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
