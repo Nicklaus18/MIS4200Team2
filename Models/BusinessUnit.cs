a55f00efbd2df03b0acb49f7ec9d357f67f1c1e4
Nick Worthington <nw173919@ohio.edu>
2022-04-14 13:09:06 -0400

    table cleaning

--- Models/BusinessUnit.cs
+++ Models/BusinessUnit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MIS4200Team2.Models
     public class BusinessUnit
     {
         public int BusinessUnitID { get; set; }
+
+        [Display(Name = "Business Unit")]
         public string unit { get; set; }
 
         ICollection<Users> Users { get; set; }
--- Models/userTitle.cs
+++ Models/userTitle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace MIS4200Team2.Models
     {
         public int usertitleID { get; set; }
 
+        [Display(Name = "Title")]
         public string titleUser { get; set; }
 
         ICollection<Users> Users { get; set; }
