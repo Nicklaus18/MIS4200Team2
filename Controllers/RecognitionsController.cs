e9f957a9561c9ab680c44971ff4f3dc7abe05cce
Nick Worthington <nw173919@ohio.edu>
2022-04-20 10:48:31 -0400

    Rec

--- Controllers/RecognitionsController.cs
+++ Controllers/RecognitionsController.cs
@@ -24,6 +24,7 @@ namespace MIS4200Team2.Controllers
         }
 
         // GET: Recognitions/Details/5
+        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -39,6 +40,7 @@ namespace MIS4200Team2.Controllers
         }
 
         // GET: Recognitions/Create
+        [Authorize]
         public ActionResult Create()
         {
             ViewBag.CoreValuesID = new SelectList(db.CoreValues, "CoreValuesID", "CoreValue");
@@ -69,6 +71,7 @@ namespace MIS4200Team2.Controllers
         }
 
         // GET: Recognitions/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,6 +93,7 @@ namespace MIS4200Team2.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Edit([Bind(Include = "RecognitionID,UsersID,CoreValuesID,descriptionOfValue")] Recognition recognition)
         {
             if (ModelState.IsValid)
@@ -104,6 +108,7 @@ namespace MIS4200Team2.Controllers
         }
 
         // GET: Recognitions/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
